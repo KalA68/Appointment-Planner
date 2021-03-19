3d9d3362f6aa52d00256d5c80edb0f9c73725ae4
KalA68 <kathyalbert1@me.com>
2021-03-19 14:07:13 +0000

    Second commit trying to fix TileList and Tile

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -2,15 +2,15 @@ import React from "react";
 
 export const Tile = (props) => {
   const data = Object.values(props);
-
   return (
     <div className="tile-container">
       {data.map( (dataItem, i) => {
         if(i === 0) {
-          return <p className='tile-title' key ={i}>{dataItem}</p>
+          return <p className='tile-title' key={i}>{dataItem}</p>
           } else {
-            return <p className='tile' key ={i}>{dataItem}</p>
-          }
+            return <p className='tile'>{dataItem}</p>
+          
+      }
       })}
     </div>
   );
--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -2,10 +2,10 @@ import React from "react";
 import {Tile} from '../tile/Tile';
 
 export const TileList = (props) => {
-const objArray = Object.values(props);
+  const objArray = Object.values(props);
   return (
     <div>
-      {objArray.map( (contact, i) => <Tile value={contact} key={i}/>)}
+      {objArray.map( (contact, i) => <Tile value={contact.name} key={i}/>)}
     </div>
   );
 };
